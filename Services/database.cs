0b74ef7feaf3c60a7200f6b4208723e53a33d12d
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-19 08:43:08 +0000

    register ui added

--- Services/database.cs
+++ Services/database.cs
@@ -6,7 +6,6 @@ using static Console;
 public class database
 {
     private string path = "Data Source=/home/bro/my-creations/live-message-app/databases/data.db ";
-
     public bool check_login(string username, string passwd)
     {
         if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(passwd))
@@ -21,6 +20,28 @@ public class database
         long res = (long)cmd.ExecuteScalar()!;
         Console.WriteLine($"DEBUG: user='{username}' pass='{passwd}' count={res}");
         return res > 0;
+    }
+
+    public bool add(string username, string name, string password, string gmail)
+    {
+        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(password) ||
+            string.IsNullOrEmpty(gmail))
+        {
+            return false;
+        }
+
+        using var con = new SqliteConnection(path);
+        con.Open();
+        var cmd = con.CreateCommand();
+        cmd.CommandText = "INSERT INTO users VALUES ($user,$name,$passwd,$gmail);";
+        cmd.Parameters.AddWithValue("$user", username);
+        cmd.Parameters.AddWithValue("$name", name);
+        cmd.Parameters.AddWithValue("$passwd", password);
+        cmd.Parameters.AddWithValue("$gmail", gmail);
+
+        long res = (long)cmd.ExecuteNonQuery()!;
+        return res > 0;
+        
     }
     
 }
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -1,10 +1,12 @@
+using System.Reflection.Metadata.Ecma335;
+
 namespace live_message_app.ViewModels;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 public partial class RegisterViewModel: ViewModelBase
 {
     private readonly MainWindowViewModel _main;
-
+    [ObservableProperty] string username,name,passwrd,cpasswd,gmail,message;
     public RegisterViewModel(MainWindowViewModel main)
     {
         _main = main;
@@ -14,5 +16,16 @@ public partial class RegisterViewModel: ViewModelBase
     {
         _main.Currentpage = new LoginViewModel(_main);
     }
-    
+
+    [RelayCommand]
+    public bool rgister()
+    {
+        if (Cpasswd != Passwrd)
+        {
+            Message = "passwords arent matching";
+        }else if (!Gmail.Contains("@gmail.com"))
+        {
+            Message = "not a valid ";
+        }
+    }
 }
