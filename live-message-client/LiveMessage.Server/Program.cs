513c90d0f78c06c79f78cf50434de805a30e9dde
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-21 22:40:39 +0000

    added the server , tested it and so far so good(System.Net.Sockets)

--- live-message-client/LiveMessage.Server/Program.cs
+++ live-message-client/LiveMessage.Server/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+
+TcpListener server = new TcpListener(IPAddress.Any, 5000);
+server.Start();
+Console.WriteLine("Server started on port 5000");
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+
+    Console.WriteLine("Client connected");
+}
